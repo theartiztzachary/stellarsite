ba0639613c945da69c7bc8e3adc7834a3437e4f3
Zachary Wilmer <43282058+theartiztzachary@users.noreply.github.com>
2025-09-09 14:46:00 -0400

    some small tweaks to try to get it working on netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "tailwindcss";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
