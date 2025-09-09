57afb408aa9a4bd9687c93e8ff3c590b68501068
Zachary Wilmer <43282058+theartiztzachary@users.noreply.github.com>
2025-09-09 16:30:35 -0400

    tweaks for netlify
    pls work :<

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "tailwindcss";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
+import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths(), netlifyPlugin()]
+  plugins: [reactRouter(), tsconfigPaths(), netlifyPlugin(), netlify()]
 });
