56223307ab2c6366787eca14eae9ea2cc51fb9ee
Zachary Wilmer <43282058+theartiztzachary@users.noreply.github.com>
2025-10-26 13:03:07 -0400

    figuring out how to pull files

--- app/components/blogheader.tsx
+++ app/components/blogheader.tsx
@@ -1,7 +1,37 @@
+import { readdir } from 'fs';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Search, Mic } from 'lucide-react';
 
 const BlogHeader = () => {
+	//constants and variables//
+	const blogDirectory = '../pages/blogpages/blog';
+
+	//internal components//
+	const SearchBar = () => {
+		return(
+
+		);
+	}; //end of SearchBar component
+
+	//internal functions//
+	async function getBlogDirectory(): [string] {
+		const pages: [string] = readdir(blogDirectory, (error, files)) => {
+			if (error) {
+				console.error('Error in pulling blog pages:', error);
+				return
+			} 
+		});
+
+		return pages;
+	}
+
+	//this will run once when a page loads, allowing us to pre-load blog directory
+	useEffect(() => {
+
+	}
+
+	//beginning of "HTML" code//
 	return(
 		<p>testing</p>
 	);
---
+++ app/pages/blogpages/blog/testpage.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import type { Route } from './+types/home';
+
+import '../../../csssheets/bloghomestyle.css';
+
+import PageHeader from '../../components/pageheader.tsx';
+import '../../../csssheets/pageheader.css';
+//import PageFooter from '../../components/pagefooter.tsx';
+//import '../../../csssheets/pagefooter.css';
+import BlogHeader from '../../components/blogheader.tsx';
+import '../../../csssheets/blogheaderstyle.css';
+
+export function meta({}: Route.MetaArgs) {
+    return [
+        { title: "Stellar Sakura - Blog" },
+        {name: "description", content: "beep" },
+    ];
+};
+
+//tags//
+export const tags = ['test'];
+
+export default function BlogHome() {
+
+//beginning of "HTML" code//
+  return (
+      <>
+      <PageHeader />
+      <BlogHeader />
+
+      <div className = "page_section">
+        <h1>this is a test page/h1>
+        <p>:)</p>
+      </div>
+
+      {/* footer goes here */}
+      </>
+  );
+}
