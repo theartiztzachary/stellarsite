fa80d1177c9eecb4eb15b93ae94d037eb4f6b08f
Zachary Wilmer <43282058+theartiztzachary@users.noreply.github.com>
2025-12-21 16:10:01 -0500

    timer works!
    next to grab images and display, and then after that need sounds!

--- app/pages/mabinogi/bripizza.tsx
+++ app/pages/mabinogi/bripizza.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import type { Route } from './+types/home';
 import { Link } from 'react-router-dom';
-import { SimpleTimer } from '../../util.tsx';
 
 import '../../../csssheets/bripizzastyle.css';
 
@@ -10,7 +9,6 @@ import '../../../csssheets/pageheader.css';
 //import PageFooter from '../../components/pagefooter.tsx';
 //import '../../../csssheets/pagefooter.css';
 
-//import useSound from 'use-sound';
 import pizzaTime from '../../../sounds/pizza-time-theme.mp3';
 import beep from '../../../sounds/price-is-right-wheel-beep.mp3';
 
@@ -22,14 +20,10 @@ export function meta({ }: Route.MetaArgs) {
 };
 
 export default function BriPizza() {
-    //sound constants//
-    //const [pizzaTimeSong, { stopPizzaTime }] = useSound(pizzaTime, {volume: 0.25,});
-    //const [beepSound] = useSound(beep);
-
     //constants and variables//
-    const timer = new SimpleTimer(2.5);
+    const intervalReference = useRef(null);
     const [currentSequence, setCurrentSequence] = useState(0); //1 is 55%, 2 is 15%
-    const [spotSequence, setSpotSequence] = useState(1); //1 - 10
+    const [spotSequence, setSpotSequence] = useState(0);
     const [currentSpot, setCurrentSpot] = useState('Click the appropriate button to begin.')
     var currentSpotIMG;
     const [nextSpot, setNextSpot] = useState('Click the appropriate button to begin.')
@@ -37,11 +31,11 @@ export default function BriPizza() {
 
     //useEffects//
     useEffect(() => {
-        if (timer.running) {
-            console.log('Spot sequence is: ' + console.log(spotSequence));
+        if (spotSequence > 0) {
+            //console.log('Spot sequence is: ' + spotSequence);
             switch (spotSequence) {
                 case 1:
-                    console.log('One!');
+                    //console.log('One!');
                     if (currentSequence == 1) {
                         setCurrentSpot('SOUTH');
                         //set current spot IMG
@@ -53,10 +47,10 @@ export default function BriPizza() {
                         setNextSpot('SOUTHEAST');
                         //set next spot IMG
                     }
+                    startTimer();
                     break;
                 case 2:
-                    console.log('Two!');
-                    //beepSound();
+                    //console.log('Two!');
                     if (currentSequence == 1) {
                         setCurrentSpot('SOUTH');
                         //set current spot IMG
@@ -70,8 +64,7 @@ export default function BriPizza() {
                     }
                     break;
                 case 3:
-                    console.log('Three!');
-                    //beepSound();
+                    //console.log('Three!');
                     if (currentSequence == 1) {
                         setCurrentSpot('WEST');
                         //set current spot IMG
@@ -85,8 +78,7 @@ export default function BriPizza() {
                     }
                     break;
                 case 4:
-                    console.log('Four!');
-                    //beepSound();
+                    //console.log('Four!');
                     if (currentSequence == 1) {
                         setCurrentSpot('NORTH');
                         //set current spot IMG
@@ -100,8 +92,7 @@ export default function BriPizza() {
                     }
                     break;
                 case 5:
-                    console.log('Five!');
-                    //beepSound();
+                    //console.log('Five!');
                     if (currentSequence == 1) {
                         setCurrentSpot('EAST');
                         //set current spot IMG
@@ -115,8 +106,7 @@ export default function BriPizza() {
                     }
                     break;
                 case 6:
-                    console.log('Six!');
-                    //beepSound();
+                    //console.log('Six!');
                     if (currentSequence == 1) {
                         setCurrentSpot('SOUTH');
                         //set current spot IMG
@@ -130,8 +120,7 @@ export default function BriPizza() {
                     }
                     break;
                 case 7:
-                    console.log('Seven!');
-                    //beepSound();
+                    //console.log('Seven!');
                     if (currentSequence == 1) {
                         setCurrentSpot('EAST');
                         //set current spot IMG
@@ -145,8 +134,7 @@ export default function BriPizza() {
                     }
                     break;
                 case 8:
-                    console.log('Eight!');
-                    //beepSound();
+                    //console.log('Eight!');
                     if (currentSequence == 1) {
                         setCurrentSpot('NORTHEAST');
                         //set current spot IMG
@@ -160,8 +148,7 @@ export default function BriPizza() {
                     }
                     break;
                 case 9:
-                    console.log('Nine!');
-                    //beepSound();
+                    //console.log('Nine!');
                     if (currentSequence == 1) {
                         setCurrentSpot('NORTH');
                         //set current spot IMG
@@ -175,8 +162,7 @@ export default function BriPizza() {
                     }
                     break;
                 case 10:
-                    console.log('Ten!');
-                    //beepSound();
+                    //console.log('Ten!');
                     if (currentSequence == 1) {
                         setCurrentSpot('NORTH');
                         //set current spot IMG
@@ -189,34 +175,42 @@ export default function BriPizza() {
                         //set next spot IMG
                     }
                     break;
-                default: //how
-                    console.log('...how');
+                default:
+                    //reset stuff
+                    //console.log('Done!');
+                    setCurrentSpot('Click the appropriate button to begin.');
+                    setNextSpot('Click the appropriate button to begin.');
+                    stopTimer();
                     break;
             };
         };
     }, [spotSequence]);
 
+    //interval clean up on unmount
     useEffect(() => {
-        console.log('Timer finished state has changed.');
-        if (timer.finished) {
-            console.log('Timer finished (bripizzahelper).');
-            if (spotSequence != 10) {
-                setSpotSequence(prevCount => prevCount + 1);
-                timer.start();
-            } else {
-                timer.reset();
-                setSpotSequence(0);
+        return () => {
+            if (intervalReference.current) {
+                clearInterval(intervalReference.current);
             }
-        }
-
-    }, [timer.finished]);
+        };
+    }, []);
 
     //functions//
-    function resetSequence() {
-        console.log('Click! Reset Sequence');
-        timer.reset();
-        setSpotSequence(0);
+    function startTimer() {
+        console.log('Timer has started...');
+        intervalReference.current = setInterval(() => {
+            setSpotSequence(prevCount => prevCount + 1);
+        }, 2500);
     };
+
+    function stopTimer() {
+        console.log('Timer has stopped...');
+        if (intervalReference.current) {
+            clearInterval(intervalReference.current);
+            intervalReference.current = null;
+        }
+        setSpotSequence(0);
+    }
 
 	return ( //'HTML' code
 		<>
@@ -231,22 +225,20 @@ export default function BriPizza() {
             <br />
             <br />
             <button onClick={() => {
-                    //pizzaTimeSong();
                     console.log('Click! 55%');
                     setCurrentSequence(1);
-                    timer.start();
+                    setSpotSequence(1);
                 }}>
                 55% Pizza
             </button>
             <button onClick={() => {
-                    //pizzaTimeSong();
                     console.log('Click! 15%');
                     setCurrentSequence(2);
-                    timer.start();
+                    setSpotSequence(1);
                 }}>
                 15% Pizza
             </button>
-            <button onClick={resetSequence}> Reset Sequence </button>
+            <button onClick={stopTimer}> Reset Sequence </button>
 
             <h2>CURRENT SPOT:</h2>
             <p>{currentSpot}</p>
