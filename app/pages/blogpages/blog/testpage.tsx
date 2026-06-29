412ce066ce157c286caf647565038e4a92748c18
Zachary Wilmer <43282058+theartiztzachary@users.noreply.github.com>
2026-06-29 02:10:52 -0400

    please

--- app/pages/blogpages/blog/testpage.tsx
+++ app/pages/blogpages/blog/testpage.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import type { Route } from './+types/home';
 
@@ -41,49 +40,4 @@ export default function TestPage() {
       <PageFooter />
       </>
   );
-=======
-import React from 'react';
-import type { Route } from './+types/home';
-
-import '../../../../csssheets/bloghomestyle.css';
-
-import PageHeader from '../../../components/pageheader.tsx';
-import '../../../../csssheets/pageheader.css';
-//import PageFooter from '../../components/pagefooter.tsx';
-//import '../../../csssheets/pagefooter.css';
-import BlogHeader from '../../../components/blogheader.tsx';
-import '../../../../csssheets/blogheaderstyle.css';
-
-export function meta({}: Route.MetaArgs) {
-    return [
-        { title: "Stellar Sakura - Blog" },
-        {name: "description", content: "beep" },
-    ];
-};
-
-//tags//
-export const name = 'Test Page';
-export const tags = ['test'];
-export const id = 'testpage';
-export const routelink = '/blog/testpage';
-export const description = 'Test description.'
-export const date = '2025-01-01T10:00:00Z'
-
-export default function TestPage() {
-
-//beginning of "HTML" code//
-  return (
-      <>
-      <PageHeader />
-      <BlogHeader />
-
-      <div className = "page_section">
-        <h1>this is a test page</h1>
-        <p>:)</p>
-      </div>
-
-      {/* footer goes here */}
-      </>
-  );
->>>>>>> master
 }
