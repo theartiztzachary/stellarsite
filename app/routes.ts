96fcff6acbb7a77eac4aee00d562bb920f3579d7
Zachary Wilmer <43282058+theartiztzachary@users.noreply.github.com>
2026-06-29 02:13:44 -0400

    how tf this this happen

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
 	type RouteConfig,
 	route,
@@ -41,42 +40,3 @@ export default [
 
 	...(await flatRoutes()),
 ] satisfies RouteConfig;
-=======
-import {
-	type RouteConfig,
-	route,
-} from "@react-router/dev/routes";
-import { flatRoutes } from "@react-router/fs-routes";
-
-export default [
-	// Home
-	route("/", "pages/home.tsx"),
-
-	// Blog
-	route("/blog", "pages/blogpages/bloghome.tsx"),
-    route("/blog/testpage", "pages/blogpages/blog/testpage.tsx"),
-    route("/blog/mabiskillcurve", "pages/blogpages/blog/mabiskillcurve.tsx"),
-
-	// Stories
-	// Copper Eye
-	route("/stories/copper_eye/overview", "pages/stories/coppereye/ceoverview.tsx"),
-
-	// Mabinogi
-    route("/mabinogi/tracker", "pages/mabinogi/mabitracker.tsx"),
-    route("/mabinogi/bripizza", "pages/mabinogi/bripizza.tsx"),
-
-	// Warframe
-	route("/warframe/tracker", "pages/warframe/wftracker.tsx"),
-
-	// Zenith and Nadir
-	route("/zenithnadir/overview", "pages/zenithnadir/znoverview.tsx"),
-
-	//Misc
-	route("/misc/wordletool", "pages/misc/wordlecheater.tsx"),
-
-    //Other Pages
-    route("/privacystatement", "pages/privacystatement.tsx"),
-
-	...(await flatRoutes()),
-] satisfies RouteConfig;
->>>>>>> master
