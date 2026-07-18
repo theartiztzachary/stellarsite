5be05e4fa4277d72ea1ee45163d5b575f497c860
Zachary Wilmer <43282058+theartiztzachary@users.noreply.github.com>
2026-07-18 13:52:38 -0400

    Z INDEX WORKS YIPPEE

--- app/pages/zenithnadir/characterbios/dejinbio.tsx
+++ app/pages/zenithnadir/characterbios/dejinbio.tsx
@@ -2,12 +2,12 @@ import type { Route } from "./+types/home";
 import { useState } from 'react';
 import ImageModal from "~/components/imagemodal";
 
-import '../../../../csssheets/znstyle.css';
-
 import PageHeader from '../../../components/pageheader.tsx';
 import '../../../../csssheets/pageheader.css';
 import PageFooter from '../../../components/pagefooter.tsx';
 import '../../../../csssheets/pagefooter.css';
+
+import '../../../../csssheets/znstyle.css';
 
 export function meta({}: Route.MetaArgs) {
     return [
